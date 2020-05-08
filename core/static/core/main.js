63424780c02bbeaacc1294047a14d3cd08d3d572
EGAMAGZ <gazapito1@hotmail.com>
2020-05-08 15:12:30 -0500

    Added design to sidenavbar

--- core/static/core/main.js
+++ core/static/core/main.js
@@ -1,4 +1,11 @@
-var openNavBtn,closeNavBtn;
-(function(){
+var openNavBtn=document.getElementById("navToggle");
 
-})();
+openNavBtn.onclick=()=>{
+    document.getElementById("sidenavbar").style.width = "250px";
+    document.getElementById("contentcontainer").style.marginLeft="250px";
+};
+
+function closeNav(){
+    document.getElementById("sidenavbar").style.width = "0";
+    document.getElementById("contentcontainer").style.marginLeft= "0";
+}
