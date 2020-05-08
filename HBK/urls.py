8112a98d0d2de1a4f59c80e5dc23c2d505e92027
EGAMAGZ <gazapito1@hotmail.com>
2020-05-08 14:25:38 -0500

    Added static paths

--- HBK/urls.py
+++ HBK/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 
 from core.views import main
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',main, name="home"),
-]
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
