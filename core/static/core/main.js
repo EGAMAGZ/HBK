661703217bb3d850ad2032cf905713ceea81ab55
EGAMAGZ <gazapito1@hotmail.com>
2020-05-08 15:43:58 -0500

    Few design changes

--- core/static/core/main.js
+++ core/static/core/main.js
@@ -1,11 +1,13 @@
 var openNavBtn=document.getElementById("navToggle");
+var closeNavBtn=document.getElementById("sidenavbar");
 
 openNavBtn.onclick=()=>{
     document.getElementById("sidenavbar").style.width = "250px";
     document.getElementById("contentcontainer").style.marginLeft="250px";
 };
 
-function closeNav(){
+closeNavBtn.onclick=()=>{
     document.getElementById("sidenavbar").style.width = "0";
     document.getElementById("contentcontainer").style.marginLeft= "0";
-}
+};
+
